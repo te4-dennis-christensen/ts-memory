11be76ae4da07eb1ccd8f27488d4838597f099be
Te4-Dennis-Christensen <dennis.christensen@elev.ga.ntig.se>
2020-11-04 12:17:51 +0100

    shuffle

--- src/game.ts
+++ src/game.ts
@@ -10,6 +10,7 @@ class Game {
         this.container = container
         const cards : string[] = ["1_pig.png", "2_squirrel.png", "3_rabbit.png", "4_frog.png", "5_fox.png", "6_bear.png", "7_monkey.png", "8_panda.png", "9_chick.png", "10_tiger.png", "11_penguin.png", "12_racoon.png"]
         this.cards = cards.map(card => new Card(card, this.cardTemplate))
+        this.cards = this.shuffle(this.cards)
     }
 
     public run(){
@@ -19,6 +20,14 @@ class Game {
 
     public click(){
 
+    }
+
+    public shuffle(list : any[]){
+        for (let i = list.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [list[i], list[j]] = [list[j], list[i]];
+        };
+        return list
     }
 }
 
